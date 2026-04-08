a2a544af549161dbab859102aaa32b0cd3a62230
andersonestrada-rgb <anderson.estrada@tecsup.edu.pe>
2026-04-08 17:44:16 -0500

    extra

--- Assets/Scripts/Pocion.cs
+++ Assets/Scripts/Pocion.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Pocion : MonoBehaviour
+public class Pocion : Collectable
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
